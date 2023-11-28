29e908beac40ff194af91d74c1f10a02029710d2
vijay vekariya <vijayvekariya007@gmail.com>
2023-11-28 09:55:43 +0530

    28 nov

--- 1.js
+++ 1.js
@@ -1,1 +1,31 @@
-console.log("javascript");
+//console.log("javascript");
+
+//document.write("Learning JS");
+
+//alert("Hello one!!!!");
+
+//HTML DOM method
+//document.getElementById("test").innerHTML = "Good Mornig..."
+
+//variable ->var, lat, const
+//Data type -> number,string, boolean, null, object, undefind
+
+//var a; // variable declared
+//var a=20; // variable iniatial
+//console.log("Value of a is: ",a);
+//a = "Hello";
+// consol.log(a);
+
+//var a = true;
+//consol.log(a);
+//consol.log(a);
+
+
+
+// let a = 20;
+// console.log(a);
+
+// a=30
+
+const b = 50;
+console.log(b);
