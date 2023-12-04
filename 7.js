1894ab3b4b74377da599796c5b0c600de1260489
rid122 <133644694+rid122@users.noreply.github.com>
2023-12-04 09:59:38 +0530

    4 Dec

---
+++ 7.js
@@ -0,0 +1,48 @@
+/* 
+     String => collection of characters(Array)
+
+*/
+
+// let txt;
+// let str = "hellow World";
+// str = 'Skill Qode';
+
+// txt = "Hellow guys' dhytggjkjkjs"
+//txt = 'Hellow guys"dhytggjkjkjs'
+// console.log(txt);
+
+
+/*  String Methods
+
+    length -> property
+    slice(start position, end pposition)
+    substring(start position, end pposition)
+    substr(start position, length)
+    toUpperCase -> upper case convert
+    toLowerCAse -> lower case convert
+    charAt -> return index character
+    charCodeAT -> retun index character ASCII value
+    contact -> merge two string
+
+*/
+
+let data = "JavaScript is the world's most popular programming language."
+
+// console.log(data.length);
+// console.log(data.slice(5, 12));       // cript i
+// console.log(data.slice(25));         // Staring to rest of all string
+// console.log(data.slice(-12, -3));   //last to counting
+
+// console.log(data.substring(5, 12));    // cript i
+// console.log(data.substring(25));      // Staring to rest of all string
+// console.log(data.substring(-12));    //counting to 0
+// console.log(data.substring(-12, 5))    //counting to 0
+
+// console.log(data.substr(5,12))   // cript is the
+// console.log(data.substr(-15, 12));  // mming langua
+// console.log(data.toUpperCase());
+// console.log(data.toLowerCase());
+// console.log(data.charAt(0))
+// console.log(data.charCodeAt(0));
+
+// console.log(data.concat("Hellow World", "\n SkillQode"));
