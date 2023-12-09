e809ef113ba5bff2fc933af9f284361314e7d38e
vijay vekariya <vijayvekariya007@gmail.com>
2023-12-09 10:03:52 +0530

    9 Dec 2023

---
+++ function.js
@@ -0,0 +1,56 @@
+//function
+
+// Hellow();       //function call
+
+// function Hellow()     //function defined
+// {
+//     console.log("Hellow World");
+// }
+
+
+
+
+// function Add(a, b)
+// {
+//     console.log(a * b);
+// }
+
+// Add(10,20);
+// Add(10, 'hellow');
+
+
+
+
+// function Product(a,b,c)
+// {
+//     return a * b * c;
+// }
+
+// // let x = Product(1, 5, 9);
+// // console.log(x)
+
+// console.log(Product(1,50,9))
+
+
+
+
+//function as variable / anonymous function
+
+// const Product = function (a, b, c)
+// {
+//     return a * b * c;
+// }
+
+// console.log(Product(11, 22, 3));
+
+
+
+
+
+// let skill = function () {
+//     console.log('SkillQode Decode Your Skill');
+// };
+
+// skill();
+
+
