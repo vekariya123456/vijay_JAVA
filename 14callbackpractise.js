4937b895aa5bf876a243cb0ab049d8256bf647e1
vijay vekariya <vijayvekariya007@gmail.com>
2023-12-20 09:51:37 +0530

    20 Dec 2023

--- 14callbackpractise.js
+++ 14callbackpractise.js
@@ -44,3 +44,26 @@
     // // calling the function
     // showTime();
 }
+
+
+
+
+{
+    // // program to stop the setTimeout() method
+
+    // let count = 0;
+
+    // // function creation
+    // function increaseCount(){
+
+    //     // increasing the count by 1
+    //     count += 1;
+    //     console.log(count)
+    // }
+
+    // let id = setTimeout(increaseCount, 3000);
+
+    // // clearTimeout
+    // // clearTimeout(id); 
+    // console.log('setTimeout is stopped.');
+}
--- callback1.js
+++ callback1.js
@@ -46,23 +46,23 @@
 // Display Time Every 3 Second
 
 {
-    // // program to display time every 3 seconds
-    function showTime() {
+//     // // program to display time every 3 seconds
+//     function showTime() {
 
-        // return new date and time
-        let dateTime= new Date();
+//         // return new date and time
+//         let dateTime= new Date();
 
-        // returns the current local time
-        let time = dateTime.toLocaleTimeString();
+//         // returns the current local time
+//         let time = dateTime.toLocaleTimeString();
 
-        console.log(time)
+//         console.log(time)
 
-        // display the time after 3 seconds
-        setTimeout(showTime, 3000);
-    }
+//         // display the time after 3 seconds
+//         setTimeout(showTime, 3000);
+//     }
 
-    // calling the function
-    showTime();
+//     // calling the function
+//     showTime();
 }
 
 // The above program displays the time every 3 seconds.
